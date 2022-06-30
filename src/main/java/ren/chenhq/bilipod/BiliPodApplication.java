ca08df8438d4f0bec9f88c51e66499ddf18e6cab
oddcc <skycc71640@gmail.com>
2022-06-30 15:27:09 +0800

    init commit

---
+++ src/main/java/ren/chenhq/bilipod/BiliPodApplication.java
@@ -0,0 +1,13 @@
+package ren.chenhq.bilipod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiliPodApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BiliPodApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ren/chenhq/bilipod/BiliPodApplicationTests.java
@@ -0,0 +1,13 @@
+package ren.chenhq.bilipod;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiliPodApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
